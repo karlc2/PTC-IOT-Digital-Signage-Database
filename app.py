ef4f4fafc77df895fce5e5d51df740bf73e1a414
karlc2 <155860384+karlc2@users.noreply.github.com>
2024-01-30 02:55:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def delete():
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
